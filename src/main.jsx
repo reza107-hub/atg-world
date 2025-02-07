48b06c17d84398dbd77bacb82e0f3cd8bd760e85
reza107-hub <rashidaminur104@gmail.com>
2025-02-07 18:48:02 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Main from "./Layout/Main/Main";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
